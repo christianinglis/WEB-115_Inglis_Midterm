7ebc9d7bdf51d9e5f83fcefebc41356c49dd6042
Christian Inglis <ccinglis@my.waketech.edu>
2026-03-24 13:05:45 -0400

    html

---
+++ script.js
@@ -0,0 +1,9 @@
+// User Input
+const initialloan = input("Enter your Initial Loan Amount (numeric, no commas): ")
+const downpayment = input("Enter Down Payment AS A PERCENTAGE: ")
+const loanterm = input("Enter Loan term in years (must be 15 or 30): ")
+
+// Initial Calculations
+    if (loanterm == 15 || loanterm == 30) {
+        
+    }
