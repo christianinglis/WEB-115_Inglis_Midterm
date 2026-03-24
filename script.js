e10a55186f51b69d78a00874361dd726c83b6fab
Christian Inglis <ccinglis@my.waketech.edu>
2026-03-24 13:37:06 -0400

    div1

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const initialloan = prompt("Enter your Initial Loan Amount (numeric, no commas): ")
 const downpayment = prompt("Enter Down Payment AS A PERCENTAGE: ")
 const loanterm = prompt("Enter Loan term in years (must be 15 or 30): ")
+const totalmonths = loanterm * 12
 
 // Initial Calculations
     if (loanterm == 15 || loanterm == 30) {
@@ -9,16 +10,17 @@ const loanterm = prompt("Enter Loan term in years (must be 15 or 30): ")
         alert("loan term must be 15 or 30 years, please try again.")
     }
 
-let principleloanamaount = initialloan - downpayment
+let principleloanamount = initialloan - downpayment
 let fixedinterestrate = 5.75
-let monthlypayment = ((monthlyinterestrate * principleloanamount) /
-(1 - Math.pow(1 + monthlyinterestrate, - totalmonths))) .toFized(2);
+let monthlyinterestrate = fixedinterestrate / 12
+let monthlypayment = ((monthlyinterestrate * principleloanamount) / (1 - Math.pow(1 + monthlyinterestrate, -totalmonths))).toFixed(2);
 
-let totalinterestpaid = (monthlypayment * totalmonths) - principleloanamaount
-let loancost = principleloanamaount + totalinterestpaid
+let totalinterestpaid = (monthlypayment * totalmonths) - principleloanamount
+let loancost = principleloanamount + totalinterestpaid
 
-const div1 = document.getElementById("initialloan")
-for (i;i++; totalmonths)
+button.addEventListener("click")
     div1.innerHTML = "The loan term is " + loanterm + "The Annual Interest Rate is " + annualinterestrate + "The Monthly Interest Rate is "
-    + monthlyinterestrate + "The Principle Loan Amount is " + principleloanamaount + "The Total interest paid is "
+    + monthlyinterestrate + "The Principle Loan Amount is " + principleloanamount + "The Total interest paid is "
     + totalinterestpaid + "The Total Loan Cost is " + totalloancost + "The Monthly Payment is " + monthlypayment
+
+let 
